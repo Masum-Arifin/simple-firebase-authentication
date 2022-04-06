a7778dfe44bca42a1def9398ba6b3edabfb6bb7e
Masum-Arifin <masumarifinbd@gmail.com>
2022-04-06 16:07:33 +0600

    new code

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,19 +1,19 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyD2AxqWNIw_P7PHihDy24UbNgg2FNzReoQ",
-  authDomain: "simple-firebase-authenti-ca221.firebaseapp.com",
-  projectId: "simple-firebase-authenti-ca221",
-  storageBucket: "simple-firebase-authenti-ca221.appspot.com",
-  messagingSenderId: "10370987120",
-  appId: "1:10370987120:web:5db68cc648ace0f1e370b5",
-};
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyD2AxqWNIw_P7PHihDy24UbNgg2FNzReoQ",
+//   authDomain: "simple-firebase-authenti-ca221.firebaseapp.com",
+//   projectId: "simple-firebase-authenti-ca221",
+//   storageBucket: "simple-firebase-authenti-ca221.appspot.com",
+//   messagingSenderId: "10370987120",
+//   appId: "1:10370987120:web:5db68cc648ace0f1e370b5",
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
 
-export default app;
+// export default app;
