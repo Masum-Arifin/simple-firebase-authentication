7b473f57f02bbdbfd2088541b6c2841e91c41a6e
Masum-Arifin <masumarifinbd@gmail.com>
2022-04-06 16:06:20 +0600

    new code

--- src/App.js
+++ src/App.js
@@ -1,46 +1,46 @@
-// import "./App.css";
-// import app from "./firebase.init";
-// import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
-// import { useState } from "react";
+import "./App.css";
+import app from "./firebase.init";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { useState } from "react";
 
-// const auth = getAuth(app);
-// function App() {
-//   const [user, setUser] = useState({})
-//   const provider = new GoogleAuthProvider();
-//   const handleGoogleSignIn = () => {
-//     signInWithPopup(auth, provider)
-//       .then((result) => {
-//         const user = result.user;
-//         setUser(user)
-//         console.log(user);
-//       })
-//       .catch((error) => {
-//         console.log(error);
-//       });
-//   };
-//   const handleSignOut = () =>{
-//      signOut(auth)
-//      .then(() =>{
-//        setUser({});
-//      })
-//      .catch(error =>{
-//       setUser({});
-//      })
-//   }
-//   return (
-//     <div className="App">
-//       {/* { condition ? true : false} */}
-//       {
-//         user.email ? <button onClick={handleSignOut}>Sign out</button>
-//         :
-//       <button onClick={handleGoogleSignIn}>Google Sign In</button>
+const auth = getAuth(app);
+function App() {
+  const [user, setUser] = useState({})
+  const provider = new GoogleAuthProvider();
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        setUser(user)
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  const handleSignOut = () =>{
+     signOut(auth)
+     .then(() =>{
+       setUser({});
+     })
+     .catch(error =>{
+      setUser({});
+     })
+  }
+  return (
+    <div className="App">
+      {/* { condition ? true : false} */}
+      {
+        user.email ? <button onClick={handleSignOut}>Sign out</button>
+        :
+      <button onClick={handleGoogleSignIn}>Google Sign In</button>
       
-//       } 
-//       <h2>Name: {user.displayName}</h2>
-//       <h4>I know your email address: {user.email}</h4>
-//       <img src={user.photoURL} alt="" />
-//     </div>
-//   );
-// }
+      } 
+      <h2>Name: {user.displayName}</h2>
+      <h4>I know your email address: {user.email}</h4>
+      <img src={user.photoURL} alt="" />
+    </div>
+  );
+}
 
-// export default App;
+export default App;
